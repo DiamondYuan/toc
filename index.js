745ff4a8b9faf7228a5fe190d9bd1f9ed182e882
DiamondYuan <admin@diamondyuan.com>
2021-11-22 01:56:35 +0800

    feat: update

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const fs = require("fs/promises");
 const path = require("path");
 
+const spaceRegex =
+  /^[\t\n\v\f\r \u00a0\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200a\u200b\u2028\u2029\u3000]*/;
+
 function format(bookToc) {
   const lines = bookToc.split("\n").map((p) => p.trimEnd());
   const res = [];
+  let space;
+  for (let i = 0; i < lines.length; i++) {
+    const match = lines[i].match(spaceRegex);
+    if (match && match[0]) {
+      space = match[0];
+      break;
+    }
+  }
+  for (let i = 0; i < lines.length; i++) {
+    let current = lines[i];
+    let spaceHeader = "";
+    while (current.slice(0, space.length) === space) {
+      current = current.replace(space, "");
+      spaceHeader = spaceHeader + "    ";
+    }
+    lines[i] = `${spaceHeader}${current}`;
+  }
   for (const iterator of lines) {
     for (let i = iterator.length - 1; i >= 0; i--) {
       if (iterator[i] === " ") {
